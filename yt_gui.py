09312716293423aa18b355d4a43c2aeba1e310b8
trjn <271082788+trjn-dev@users.noreply.github.com>
2026-03-26 18:14:54 +0530

    Testing Update System

--- yt_gui.py
+++ yt_gui.py
@@ -15,7 +15,7 @@ import requests
 
 import yt3
 
-CURRENT_VERSION = "1.0.4"
+CURRENT_VERSION = "1.0.5"
 GITHUB_RELEASES_LATEST_URL = (
     "https://api.github.com/repos/trjn-dev/YoutubeDownloader/releases/latest"
 )
